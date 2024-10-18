42227ce2583b8e0da1e50a7c5d760ad6d704d3be
agorohov <aleksandr.gorohov@gmail.com>
2024-10-18 17:25:49 +0300

    initial commit / бот отвечает на команду "/start"

---
+++ src/main/java/com/agorohov/notificationbot/NotificationBotApplication.java
@@ -0,0 +1,15 @@
+package com.agorohov.notificationbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class NotificationBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationBotApplication.class, args);
+    }
+
+}
