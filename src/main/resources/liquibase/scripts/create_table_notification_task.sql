e0b59bbc52a10dcf991b7ed9ddb0fc11eec8481c
agorohov <aleksandr.gorohov@gmail.com>
2024-10-21 17:08:22 +0300

    Исправление ошибок

--- src/main/resources/liquibase/scripts/create_table_notification_task.sql
+++ src/main/resources/liquibase/scripts/create_table_notification_task.sql
@@ -2,7 +2,7 @@
 
 -- changeset agorohov:1
 CREATE TABLE notification_task (
-id SERIAL PRIMARY KEY,
+id BIGSERIAL PRIMARY KEY,
 chat_id BIGINT,
 message_text TEXT,
 sending_time TIMESTAMP WITH TIME ZONE
